536db61ff05ae08a1089dd89cde9df56bf1138ab
WilhelmLenz <wilhelm.lenz@neteleven.de>
2022-04-21 02:52:11 +0200

    updated app

---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// document.getElementById('moocs').addEventListener('click', () => {
+//   const a = document.createElement('a');
+//   a.setAttribute('href', '../html/moocs.html')
+// })
+
